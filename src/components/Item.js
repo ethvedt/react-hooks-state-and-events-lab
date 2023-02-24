9ae5232a01ffb705ae85f64a9f7ea8585a8b7038
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-24 11:35:03 -0800

    passed tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false);
+
+  const cartClass = inCart ? "in-cart" : "";
+
+  const buttonText = inCart ? "Remove From Cart" : "Add To Cart";
+
+  function handleClick() {
+    setInCart((inCart) => !inCart);
+
+  };
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredItems = items.filter(item => {
+    if (selectedCategory === "All") {
+      return true;
+    }
+    else {
+      return item.category === selectedCategory;
+    }
+  });
+
+  function handleChange(e) {
+    setSelectedCategory(e.target.value);
+
+    return (
+      <select onChange={handleChange} name="filter">
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+    )
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +38,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
